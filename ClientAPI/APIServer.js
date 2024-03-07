a78c5ad004555a3c250aec7a89f4cb5a6269ab3b
Shu-YuanYang <zantgoron@gmail.com>
2024-03-07 18:01:03 +0000

    Added federated scheduling file download API.

--- ClientAPI/APIServer.js
+++ ClientAPI/APIServer.js
@@ -1,6 +1,56 @@
 const express = require("express");
-const app = express();
+const cors = require("cors");
 const { exec } = require("child_process");
+const path = require("path");
+const JSZip = require('jszip');
+const fs = require('fs');
+
+const app = express();
+
+
+
+
+
+function zip_files_unsafe(zip_id, file_folder_path, filename_list, on_finish) {
+	const zip = new JSZip();
+	try {
+		for (let i = 0; i < filename_list.length; ++i) {
+			const file_full_path = `${file_folder_path}/${filename_list[i]}`;
+			if (!fs.existsSync(file_full_path)) throw new Error('file does not exist: ' + file_full_path);
+			const fileData = fs.readFileSync(file_full_path);
+			zip.file(filename_list[i], fileData);
+		}
+
+		zip.generateNodeStream({ type: 'nodebuffer', streamFiles: true })
+			.pipe(fs.createWriteStream(`${file_folder_path}/${zip_id}.zip`))
+			.on('finish', function () { console.log(`${zip_id}.zip written.`); on_finish(zip_id); });
+	} catch (err) {
+		console.error(err);
+	}
+}
+
+
+function zip_files(zip_id, file_folder_path, filename_list, on_finish) {
+	const zip_full_path = `${file_folder_path}/${zip_id}.zip`;
+	if (fs.existsSync(zip_full_path)) {
+		//console.log("unlink")
+		fs.unlink(zip_full_path, (err) => {
+			if (err) { console.error(err); return; }
+			zip_files_unsafe(zip_id, file_folder_path, filename_list, on_finish);
+		});
+	}
+	else {
+		//console.log("zip");
+		zip_files_unsafe(zip_id, file_folder_path, filename_list, on_finish);
+	}
+}
+
+
+
+
+
+// Enable CORS:
+app.use(cors());
 
 
 // Enable json response, from: https://stackoverflow.com/questions/19696240/proper-way-to-return-json-using-node-or-express
@@ -22,24 +72,43 @@ app.get("/hello", function(req, res) {
         res.json({ status: "ok", message: "hello test reqeust!" });
 });
 
-app.get("/federated_scheduling_test", function(req, res){
+
+
+const SCHEDULING_PROGRAM_FOLDER = "../NodeTimer";
+const SCHEDULING_TASK_FILES_FOLDER = `${SCHEDULING_PROGRAM_FOLDER}/task_files`; 
+function validate_scheduling_test_query(task_set_ID, processor_count) {
+	if (task_set_ID.includes("/")){
+                //res.json({ status: "rejected", message: "task ID must not include special characters!" });
+                return { status: "rejected", message: "task ID must not include special characters!" };
+        }
+        if (!parseInt(processor_count)){
+                //res.json({ status: "rejected", message: "processor count must be an integer!" });
+                return { status: "rejected", message: "processor count must be an integer!" };
+        }
+	return { status: "accepted", message: "input validated" };
+}
+
+app.post("/federated_scheduling_test", function(req, res){
 	var task_set_ID = req.query.TaskSetID;
         var processor_count = req.query.ProcessorCount;
-        console.log(task_set_ID);
-	console.log(processor_count);
-	if (task_set_ID.includes("/")){
+        //console.log(task_set_ID);
+	//console.log(processor_count);
+	//console.log(req.body);
+	validation_status = validate_scheduling_test_query(task_set_ID, processor_count);
+	if (validation_status.status != "accepted") res.json(validation_status);
+	/*if (task_set_ID.includes("/")){
 		res.json({ status: "rejected", message: "task ID must not include special characters!" });
 		return;
 	}
 	if (!parseInt(processor_count)){
 		res.json({ status: "rejected", message: "processor count must be an integer!" });
 		return;
-	}
+	}*/
 
-	var execution_str = `../NodeTimer/NodeTimer "../NodeTimer/task_files/${task_set_ID}.json" ${processor_count}`;
+	var execution_str = `${SCHEDULING_PROGRAM_FOLDER}/NodeTimer "${SCHEDULING_TASK_FILES_FOLDER}/${task_set_ID}.json" ${processor_count}`;
 
 	exec(execution_str, (error, stdout, stderr) => {
-		console.log(stdout);
+		//console.log(stdout);
 		if (error) {
 			console.log(error);
 			console.log(stderr);
@@ -48,24 +117,30 @@ app.get("/federated_scheduling_test", function(req, res){
 	});
 });
 
-/*
-app.post("/query/query_str", function(req, res){
-                var queryID = req.query.queryID;
-                var query_body = req.body;
-                var url_address = "https://6dvsvj47eujpghwa6xxxksnzi40oqxzl.lambda-url.us-east-2.on.aws/visualdata/query_str?queryID=" + queryID;
-                //console.log(JSON.stringify(query_body));
-                fetch(url_address, {
-                        method: 'post',
-                        headers: { 
-                                "Content-Type": "application/json"
-                        },
-                        body: JSON.stringify(query_body)
-                })
-                .then(CloudSharpResult => CloudSharpResult.json())
-                .then(result => { res.json(result); })
-                .catch(err => { res.sendStatus(501); });
+
+app.post("/federated_scheduling_download", function(req, res){
+	var task_set_ID = req.query.TaskSetID;
+        var processor_count = req.query.ProcessorCount;
+        //console.log(task_set_ID);
+        //console.log(processor_count);
+        //console.log(req.body);
+        validation_status = validate_scheduling_test_query(task_set_ID, processor_count);
+        if (validation_status.status != "accepted") res.json(validation_status);
+
+	var filename_list = [];
+	if (req.body.download_task_set) { 
+		filename_list.push(`${task_set_ID}.json`);
+	}
+	if (req.body.download_schedule) {
+		filename_list.push(`${task_set_ID}.csv`);
+	}
+	zip_files(task_set_ID, SCHEDULING_TASK_FILES_FOLDER, filename_list, function(zip_id) {
+                var p = path.join(__dirname, `${SCHEDULING_TASK_FILES_FOLDER}/${zip_id}.zip`);
+                //console.log(p);
+                res.sendFile(p);
+	});
 });
-*/
+
 
 
 
